ea03b410d57468c7542930e16c6a3945508e92f0
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-10-11 15:46:30 +0530

    feat: manifest and ui

--- src/react/components/App.tsx
+++ src/react/components/App.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-
-const dogSrc = "https://media.tenor.com/fej4_qoxdHYAAAAM/cute-puppy.gif";
+import { useState } from "react";
 
 const App = () => {
-  const generateDogGif = () => {
-    chrome.runtime.sendMessage({ action: "generateDogGif", dogSrc });
+  const fetchBookmarks = () => {
+    try {
+      chrome.runtime.sendMessage({ action: "fetchBookmarks" });
+      setIsFetched(true);
+    } catch (error) {
+      alert("Error fetching bookmarks");
+    }
   };
-
+  const exportBookmarks = () => {
+    chrome.runtime.sendMessage({ action: "exportBookmarks" });
+  };
+  const [isFetched, setIsFetched] = useState(false);
   return (
     <main>
-      <h1>Add a Dog Gif to Webpage</h1>
-      <img src={dogSrc} alt="Dog Gif" />
-      <button onClick={generateDogGif}>Generate Dog Gif</button>
+      <h1>BookmarkX</h1>
+
+      <button onClick={fetchBookmarks}>Fetch Bookmarks</button>
+      <button onClick={exportBookmarks} disabled={!isFetched}>
+        Export Bookmarks
+      </button>
     </main>
   );
 };
 
 export default App;
+
+//generateDogGif
